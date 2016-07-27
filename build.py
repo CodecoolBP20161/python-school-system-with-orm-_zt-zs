e252de6da24bcd6634b313c38567c613e2199068
hamargyuri <gyurinak@gmail.com>
2016-07-27 15:02:57 +0200

    fixed pep8 incompatibility

--- build.py
+++ build.py
@@ -6,7 +6,7 @@ from models import *
 db.connect()
 # Delete existing tables to avoid conflicts
 db.drop_tables([Applicant, School, City, Mentor, InterviewSlot,
-                  Interview, Question, Answer], safe=True)
+                Interview, Question, Answer], safe=True)
 # List the tables here what you want to create...
 db.create_tables([Applicant, School, City, Mentor, InterviewSlot,
                   Interview, Question, Answer], safe=True)
