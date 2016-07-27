8749b00dc23dd9fc36febf6ae7780af44f30f478
cickib <bellakocsis06@gmail.com>
2016-07-27 13:06:57 +0200

    drop table before create added

--- build.py
+++ build.py
@@ -2,7 +2,11 @@
 
 from models import *
 
+
 db.connect()
+# Delete existing tables to avoid conflicts
+db.drop_tables([Applicant, School, City, Mentor, InterviewSlot,
+                  Interview, Question, Answer], safe=True)
 # List the tables here what you want to create...
 db.create_tables([Applicant, School, City, Mentor, InterviewSlot,
                   Interview, Question, Answer], safe=True)
