6e01d97f80b0a7a25354620908abb8b130817e7a
cickib <bellakocsis06@gmail.com>
2016-09-06 11:35:07 +0200

    database connection working, new applicant added to the table

--- app.py
+++ app.py
@@ -1,10 +1,25 @@
 from flask import *
+from models import *
+import requests
 
 
-debug = True
+DEBUG = True
 
 app = Flask(__name__)
 app.config.from_object(__name__)
+
+
+
+@app.before_request
+def before_request():
+    g.database = db
+    g.database.connect()
+
+
+@app.after_request
+def after_request(response):
+    g.database.close()
+    return response
 
 
 @app.route('/')
@@ -12,9 +27,20 @@ def main():
     return render_template('main.html')
 
 
-@app.route('/registration_route')
+@app.route('/registration_route', methods=['GET', 'POST'])
 def registrate():
-    return 'Registration form goes here'
+    if (request.method == 'POST' and request.form["first_name"] and request.form["last_name"] and
+        request.form["email"] and request.form["city"]):
+        new_applicant = Applicant.create(
+            first_name=request.form["first_name"],
+            last_name=request.form["last_name"],
+            email=request.form["email"],
+            city=request.form["city"])
+        # new_applicant.save()
+        return redirect('/')
+    else:
+        # return 'Registration form goes here'
+        return render_template('registration.html')
 
 
 if __name__ == '__main__':
