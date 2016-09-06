72c09440fb224dd2fe6f13fcaeb71b4b66ffa9ae
arvisura87 <jakab.tamas@me.com>
2016-09-06 13:32:55 +0200

    html main page finished, images folder added with two pics

--- app.py
+++ app.py
@@ -3,13 +3,13 @@ from flask import *
 
 debug = True
 
-app = Flask(__name__)
+app = Flask(__name__, static_folder='images')
 app.config.from_object(__name__)
 
 
 @app.route('/')
 def main():
-    return render_template('main.html')
+    return render_template('index.html')
 
 
 @app.route('/registration_route')
