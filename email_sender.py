060fcfd9809a8d0a981505e9d3d4fec99850c97b
cickib <bellakocsis06@gmail.com>
2016-09-07 14:47:25 +0200

    ugly, but working solution

--- email_sender.py
+++ email_sender.py
@@ -1,63 +1,79 @@
 import smtplib
 from getpass import getpass
 from models import *
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 
 class Email_sender:
+
     def __init__(self, subject, recipient):
         self.subject = subject
         self.recipient = recipient
-        self.body = Datas.create_email_body()
+
+
+    @staticmethod
+    def get_applicant_data():
+        new_applicants = list(Applicant.select().where(Applicant.status == "New"))
+        applicant_datas = []
+        for applicant in new_applicants:
+            applicant_datas.append(([applicant.first_name, applicant.last_name, applicant.school.location,
+                                     applicant.application_code, applicant.status], applicant.email))
+        return applicant_datas
 
     def send_it(self):
-        sender = input("Please enter your email address: ")
-        password = getpass()
+        data = self.get_applicant_data()
+        # sender = input("Please enter your email address: ")
+        sender = "atelon09@gmail.com"
+        # password = getpass()
+        password = "10qwert01"
         count = 0
+
+        you = "atelon09@gmail.com"
+
+
         try:
-            for b in self.body:
-                msg = "\r\n".join(["From: {}".format(sender), "To: {}".format(self.recipient),
-                                   "Subject: {}".format(self.subject), "{}".format(b)])
-                try:
-                    if msg:
-                        server = smtplib.SMTP('smtp.gmail.com:587')
-                        server.ehlo()
-                        server.starttls()
-                        server.login(sender, password)
-                        server.sendmail(sender, self.recipient, msg)
-                        server.quit()
-                        count += 1
-                except:
-                    print("An error occured, email not sent.")
+            for d in data:
+                html = """\
+                <html>
+                    <head>
+                        <title>GTFO!</title>
+                    </head>
+                    <body>
+                        <img src="https://github.com/CodecoolBP20161/python-school-system-with-orm-_zt-zs/blob/email_html/static/cc_logo-large.png?raw=true">
+                        <p>Hey u peace of shit loser {} {} !</p>
+                        <br>
+                        <p>How did u even got the idea to apply to the mighty Codecool {} ?? Were u out of ur stupid mind?</p>
+                        <p>Ur status is obv '{}' . Forget about ur ridiculous name, from now on ur called {}.</p>
+                        <p>Get ur shit together, and if u can find some courage in ur meaningless self for once in ur pathetic life,</p>
+                        <p>and if for some unthinkable reason we decide u worth it were gonna send u an interview date later, so u can</p>
+                        <p>show us ur ugly face at Codecool {} .</p><br>
+                        <br>
+                        <p>Until then, continue to waste our precious air for the last time,</p>
+                        <p>Trainers at Codecool {}</p>
+                    </body>
+                </html>
+                """.format(d[0][0], d[0][1], d[0][2], d[0][4], d[0][3], d[0][2], d[0][2])
+
+                part2 = MIMEText(html, 'html')
+                # msg = "\r\n".join(["From: {}".format(sender), "To: {}".format(self.recipient),
+                #                        "Subject: {}".format(self.subject), "{}".format(html)])
+                msg = MIMEMultipart('alternative')
+                msg['Subject'] = "Your Application"
+                msg['From'] = sender
+                msg['To'] = you
+                if msg:
+                    msg.attach(part2)
+                    server = smtplib.SMTP('smtp.gmail.com:587')
+                    server.ehlo()
+                    server.starttls()
+                    server.login(sender, password)
+                    server.sendmail(sender, you, msg.as_string())
+                    server.quit()
+                    count += 1
+
         finally:
-            print("{} email(s) successfully sent to {}.".format(count, self.recipient))
+            print("{} email(s) successfully sent.".format(count))
 
 
-class Datas:
-    def __init__(self, first_name, last_name, application_code, school, status):
-        self.first_name = first_name
-        self.last_name = last_name
-        self.application_code = application_code
-        self.school = school
-        self.status = status
-
-    @staticmethod
-    def get_data():
-        email_list = []
-        applicants = list(Applicant.select())
-        for a in applicants:
-            email_list.append(Datas(a.first_name, a.last_name, a.application_code, a.school.location, a.status))
-        return email_list
-
-    @staticmethod
-    def create_email_body():
-        bodies = []
-        for person in Datas.get_data():
-            body = "Hi {} {}! Your application code is {} in Codecool {}, and your status is {}.".format(person.first_name,
-                                                                                                         person.last_name, person.application_code, person.school, person.status)
-            bodies.append(body)
-        return bodies
-
-
-
-test = Email_sender("testing", "atelon09@gmail.com")
 test.send_it()
